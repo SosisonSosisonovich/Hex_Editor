e9bfbdf0951272000e86005144d3b5de14fe49a4
Julia Golubkova <golubkovjul@gmail.com>
2024-03-30 18:32:28 +0300

    add pom.xml delete hello world

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             GUI gui = new GUI();
-            System.out.println("Hello World");
         });
     }
 }
