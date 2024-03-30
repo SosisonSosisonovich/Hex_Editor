9f8e617b20764b806a01546af441cd7d79063e64
Julia Golubkova <golubkovjul@gmail.com>
2024-03-31 00:22:10 +0300

    edit ByteArray

--- src/ByteArray.java
+++ src/ByteArray.java
@@ -1,43 +1,23 @@
 import java.io.*;
 
 public class ByteArray {
-
     private byte[] byteArray;
 
-    public ByteArray(int size){
-        byteArray = new byte [size];
-    }
-
-    public ByteArray(String file) throws IOException {
-        this(new File(file));
-    }
-
-    //считывает данные из файла и превращает в массив
-     public ByteArray(File file) throws IOException{
+    public ByteArray(File file) throws IOException {
         int size = (int) file.length();
         int offset = 0;
         byteArray = new byte[size];
 
         try (BufferedInputStream input = new BufferedInputStream(new FileInputStream(file))) {
+            byte[] buffer = new byte[4096];
             int index = 0;
             int count = 0;
             // count принимает значение кол-ва прочитанных байт или -1, если конец файла.
-            while (index < size && (count = input.read(byteArray, index, byteArray.length - index)) > -1) {
+            while (index < size && (count = input.read(buffer, 0, 4096)) > -1){
+                System.arraycopy(buffer, 0, byteArray, index, count);
                 index += count;
             }
         }
-    }
-
-    //превращает считанные из потока данные в массив байтов
-    public ByteArray(InputStream in) throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        byteArray = new byte[4096]; // создается временный массив для чтения файла блоками по 4 кбайт
-        int count = 0;
-        // count принимает значение кол-ва прочитанных байт или -1, если конец файла.
-        while ((count=in.read(byteArray, 0,byteArray.length))>-1) {
-            baos.write(byteArray, 0,count);
-        }
-        byteArray = baos.toByteArray();
     }
 
     public byte getByte(int offset) {
